33c1d0c527de1d463f2374ab6e2be302ff2f5b2f
root <root@DESKTOP-D1MON25.localdomain>
2025-07-07 20:51:59 +0100

    New ones

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import 'mapbox-gl/dist/mapbox-gl.css'; // Temporarily disabled - install mapbox-gl package to enable
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
