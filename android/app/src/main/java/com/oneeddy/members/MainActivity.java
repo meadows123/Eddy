e4f2c31cbe5dd37821076ab4c774ff0a75676417
root <root@DESKTOP-D1MON25.localdomain>
2025-07-24 09:12:00 +0100

    Remove invalid .nvmrc:Zone.Identifier file

---
+++ android/app/src/main/java/com/oneeddy/members/MainActivity.java
@@ -0,0 +1,5 @@
+package com.oneeddy.members;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.oneeddy.members',
+  appName: 'Eddy',
+  webDir: 'dist'
+};
+
+export default config;
