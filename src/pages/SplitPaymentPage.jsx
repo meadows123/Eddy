fc36434b928dee7cbd7627a3e0acea0c2c21eccd
meadows123 <zakmeadows1@hotmail.com>
2025-09-03 08:02:14 +0100

    Last emails I am trying to fix

--- src/pages/SplitPaymentPage.jsx
+++ src/pages/SplitPaymentPage.jsx
@@ -249,7 +249,7 @@ const SplitPaymentPage = () => {
               contact_email,
               contact_phone
             ),
-            venue_tables!inner(venue_id=venues.id) (
+            venue_tables!inner(venue_id.eq.venues.id) (
               table_number
             )
           )
